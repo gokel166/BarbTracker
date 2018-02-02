2cc452f3477400583e0e4543ed67294daa2ba3b9
gokel166 <okellogeorge40@yahoo.com>
2018-02-02 00:28:37 -0600

    add new fields appointments and barber id

--- backend/cust_tracker/models.py
+++ backend/cust_tracker/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 # Create your models here.
 class CustTracker(models.Model):
-    cust_id = models.Integer(null=False)
+    cust_id = models.Integer(primary_key=True, null=False)
     cust_email = models.CharField(max_length=120, null=False)
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50)
+    barber_id = models.ForeignKey(User, related_name='host_set')
+    appointment
